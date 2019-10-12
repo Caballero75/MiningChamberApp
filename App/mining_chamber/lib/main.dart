1443996a9a185a5487a78b00c7cd0c7f04e02620
Caballero75 <43910549+Caballero75@users.noreply.github.com>
2019-10-12 11:59:10 -0300

    Update main.dart

--- App/mining_chamber/lib/main.dart
+++ App/mining_chamber/lib/main.dart
@@ -33,106 +33,107 @@ class _MyHomePageState extends State<MyHomePage> {
     getTab();
     return Scaffold(
       body: Container(
-          child: Stack(
-            children: <Widget>[
-              Container(
-                padding: EdgeInsets.only(left: 20, bottom: 20, right: 20, top: 60),
-                margin: EdgeInsets.only(top: 219),
-                constraints: BoxConstraints.expand(height: MediaQuery.of(context).size.height - 219),
-                decoration: BoxDecoration(
+        child: Stack(
+          children: <Widget>[
+            Container(
+              padding:
+                  EdgeInsets.only(left: 20, bottom: 20, right: 20, top: 60),
+              margin: EdgeInsets.only(top: 219),
+              constraints: BoxConstraints.expand(
+                height: MediaQuery.of(context).size.height - 219,
+              ),
+              decoration: BoxDecoration(
                   color: darkBlueColor,
                   gradient: LinearGradient(
                     colors: [darkBlueColor, lightBlueColor],
                     stops: [0.5, 1],
-                    begin: const FractionalOffset(0.5,0.2),
+                    begin: const FractionalOffset(0.5, 0.2),
                     end: const FractionalOffset(1, 1),
                     // center: Alignment(0.0, 1),
                   ),
                   borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(30), 
-                    topRight: Radius.circular(30)
-                  ),
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30)),
                   boxShadow: [
                     new BoxShadow(
-                        color: Colors.grey,
-                        blurRadius: 20.0,
+                      color: Colors.grey,
+                      blurRadius: 20.0,
+                    ),
+                  ]),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  tab,
+                  Container(
+                    child: Center(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          Container(
+                            height: 80,
+                            width: 80,
+                            child: FloatingActionButton(
+                              child: Icon(Icons.library_books),
+                              onPressed: () {
+                                setState(() {
+                                  library = true;
+                                  services = false;
+                                  lottery = false;
+                                  getTab();
+                                });
+                              },
+                              backgroundColor: darkBlueColor,
+                            ),
+                          ),
+                          Container(
+                            height: 80,
+                            width: 80,
+                            child: FloatingActionButton(
+                              child: Icon(Icons.monetization_on),
+                              onPressed: () {
+                                setState(() {
+                                  library = false;
+                                  services = false;
+                                  lottery = true;
+                                  getTab();
+                                });
+                              },
+                              backgroundColor: darkBlueColor,
+                            ),
+                          ),
+                          Container(
+                            height: 80,
+                            width: 80,
+                            child: FloatingActionButton(
+                              child: Icon(Icons.settings),
+                              onPressed: () {
+                                setState(() {
+                                  library = false;
+                                  services = true;
+                                  lottery = false;
+                                  getTab();
+                                });
+                              },
+                              backgroundColor: darkBlueColor,
+                            ),
+                          ),
+                        ],
                       ),
-                  ]
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    tab,
-                    Container(
-                      child: Center(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: <Widget>[
-                            Container(
-                              height: 80,
-                              width: 80,
-                              child: FloatingActionButton(
-                                child: Icon(Icons.library_books),
-                                onPressed: () {
-                                  setState(() {
-                                     library = true;
-                                     services = false;
-                                     lottery = false; 
-                                     getTab();                                
-                                  });
-                                },
-                                backgroundColor: darkBlueColor,
-                              ),
-                            ),
-                            Container(
-                              height: 80,
-                              width: 80,
-                              child: FloatingActionButton(
-                                child: Icon(Icons.monetization_on),
-                                onPressed: () {
-                                  setState(() {
-                                     library = false;
-                                     services = false;
-                                     lottery = true; 
-                                     getTab();                                
-                                  });
-                                },
-                                backgroundColor: darkBlueColor,
-                              ),
-                            ),
-                            Container(
-                              height: 80,
-                              width: 80,
-                              child: FloatingActionButton(
-                                child: Icon(Icons.settings),
-                                onPressed: () {
-                                  setState(() {
-                                     library = false;
-                                     services = true;
-                                     lottery = false; 
-                                     getTab();                                
-                                  });
-                                },
-                                backgroundColor: darkBlueColor,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      height: MediaQuery.of(context).size.height *0.08,
-                    )
-                  ],
-                ),
+                    ),
+                    height: MediaQuery.of(context).size.height * 0.08,
+                  )
+                ],
               ),
-              Container(
-                padding: EdgeInsets.only(left:20, top: 130),
-                height: 219,
-                width: MediaQuery.of(context).size.width,
-                child: Text("MINING\nCHAMBER", style: pageTitleStyle),
-              ),
-            ],
-          ),
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 20, top: 130),
+              height: 219,
+              width: MediaQuery.of(context).size.width,
+              child: Text("MINING\nCHAMBER", style: pageTitleStyle),
+            ),
+          ],
         ),
+      ),
     );
   }
 
@@ -164,7 +165,10 @@ class _MyHomePageState extends State<MyHomePage> {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
-        Text("Crypto Library", style: tabTitleStyle,),
+        Text(
+          "Crypto Library",
+          style: tabTitleStyle,
+        ),
         Container(
           height: MediaQuery.of(context).size.height * 0.5,
           child: ListView(
@@ -192,8 +196,14 @@ class _MyHomePageState extends State<MyHomePage> {
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Divider(color: lightBlueColor, thickness: 1,),
-          Text("Basics of Wattage and\nElectricity", style: blogTitleStyle,),
+          Divider(
+            color: lightBlueColor,
+            thickness: 1,
+          ),
+          Text(
+            "Basics of Wattage and\nElectricity",
+            style: blogTitleStyle,
+          ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
@@ -201,11 +211,11 @@ class _MyHomePageState extends State<MyHomePage> {
               RaisedButton(
                 color: Colors.white,
                 child: Text("Read More", style: smallButtonTextStyle),
-                onPressed: () {
-
-                },
-                shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0))
-              )
+                onPressed: () {},
+                shape: RoundedRectangleBorder(
+                  borderRadius: new BorderRadius.circular(30.0),
+                ),
+              ),
             ],
           )
         ],
