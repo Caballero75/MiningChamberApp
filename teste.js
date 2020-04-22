a38f813942525854d3ad93a6b64ff5b3c4930ca2
Caballero75 <43910549+Caballero75@users.noreply.github.com>
2020-04-22 03:01:03 -0300

    Update teste.js

--- teste.js
+++ teste.js
@@ -1,3 +1,9 @@
-<script>
+// ==UserScript==
+// @name         InstaShorts
+// @namespace    http://diego.com
+// @version      0.1
+// @description  create shortcuts for web pages.
+// @author       Diego Carvalho
+// @grant        none
+// ==/UserScript==
 alert('teste');
-</script>
